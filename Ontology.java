eabf4db5c67d16807fcc5bb105caf09df2d2f1af
Angela Kuo <angela@simplyinsured.com>
2016-08-08 15:46:04 -0700

    populated topic tree

--- Ontology.java
+++ Ontology.java
@@ -2,22 +2,87 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Ontology {
+	private static TopicNode<String> _topics;
 
-	private static void readFile(String fileName) {
+	public static void makeTree(String flat) {
+		System.out.println(flat);
+		LinkedList<String> flatArray = new LinkedList<String>(Arrays.asList(flat.split(" ")));
+		if (flatArray.size() >= 1) {
+			_topics = makeTreeHelper(flatArray);
+			_topics.printTree();
+		} else {
+			_topics = new TopicNode<String>(null);
+		}
+	}
 
-        BufferedReader reader = null;
+	/**
+	*	Will return a tree with a root and its children. The head node at arraySoFar should be
+	*	the root of the tree node. arraySoFar should never start with "(" or ")" because it 
+	*	should be caught in the previous call
+	*/
+	private static TopicNode<String> makeTreeHelper(LinkedList<String> arraySoFar) {
+		TopicNode<String> treeRoot = new TopicNode<String>(arraySoFar.remove());
+		
+		// collect children
+		if (arraySoFar.get(0).equals("(")) {
+			arraySoFar.remove();
+			while (!arraySoFar.get(0).equals(")")) {
+				treeRoot.addChild(makeTreeHelper(arraySoFar));
+			}
+			arraySoFar.remove();
+		}
+		return treeRoot;
+	}
 
+	public static class TopicNode<T> {
+	    private T data;
+	    private ArrayList<TopicNode<T>> children;
+
+	    public TopicNode(T rootData) {
+	        data = rootData;
+	        children = new ArrayList<TopicNode<T>>();
+	    }
+
+	    public void printTree(){
+	    	System.out.println(data);
+	    	for (int i = 0; i < children.size(); i++) {
+				children.get(i).printTree();
+			}
+	    }
+
+        public void addChild(TopicNode<T> child) {
+        	children.add(child);
+        }
+	}
+
+    public static void main(String[] args) {
+		BufferedReader reader = null;
 		try {
-		    File file = new File(fileName);
-		    reader = new BufferedReader(new FileReader(fileName));
+		    File file = new File(args[0]);
+		    reader = new BufferedReader(new FileReader(file));
+		    // line 1			int N
+		    int n = Integer.parseInt(reader.readLine());
+		    System.out.println("N = "+n);
+		    // line 2			flat tree of N topics
+		    makeTree(reader.readLine());
+		    // line 3			int M
 
+		    // line 4..M+3		topic (colon): question text
+
+		    // line M+4			int K
+
+		    // line M+5..M+K+4	topic name (space) query text
+
+		    
+		    // reader.readLine();
+		    System.out.println("------------");
 		    String line;
 		    while ((line = reader.readLine()) != null) {
 		        System.out.println(line);
 		    }
-
 		} catch (IOException e) {
 		    e.printStackTrace();
 		} finally {
@@ -28,9 +93,5 @@ public class Ontology {
 		    }
 		}
 	}
-
-    public static void main(String[] args) {
-    	readFile("sample_file.txt");
-    }
-
 }
+
