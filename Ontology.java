0e4acbe0e72c48af46e8f2e99250ff52789e8c2b
Angela Kuo <angela@simplyinsured.com>
2016-08-10 00:08:05 -0700

    added timer

--- Ontology.java
+++ Ontology.java
@@ -3,26 +3,18 @@ import java.util.*;
 
 public class Ontology {
 	private static TopicNode<String> _topics;
-	private static HashMap<String, TopicNode<String>> _map;
+	private static HashMap<String, HashSet<String>> _map;
 	private static Trie _trie;
 
 	public static void makeTree(String flat) {
 		LinkedList<String> flatArray = new LinkedList<String>(Arrays.asList(flat.split(" ")));
 		if (flatArray.size() >= 1) {
 			_topics = makeTreeHelper(flatArray);
-		} else {
-			_topics = new TopicNode<String>(null);
 		}
 	}
 
-	/**
-	*	Will return a tree with a root and its children. The head node at arraySoFar should be
-	*	the root of the tree node. arraySoFar should never start with "(" or ")" because it 
-	*	should be caught in the previous call
-	*/
 	private static TopicNode<String> makeTreeHelper(LinkedList<String> arraySoFar) {
 		TopicNode<String> treeRoot = new TopicNode<String>(arraySoFar.remove());
-		_map.put(treeRoot.data, treeRoot);
 
 		// collect children
 		if (arraySoFar.get(0).equals("(")) {
@@ -32,63 +24,48 @@ public class Ontology {
 			}
 			arraySoFar.remove();
 		}
+
+		_map.put(treeRoot.data, treeRoot.allChildren);
 		return treeRoot;
 	}
 
 	public static class TopicNode<T> {
-	    private T data;
-	    private ArrayList<TopicNode<T>> children;
-	    private HashSet<T> allChildren;
+	    private String data;
+	    private HashSet<String> allChildren;
 
-	    public TopicNode(T rootData) {
+	    public TopicNode(String rootData) {
 	        this.data = rootData;
-	        this.children = new ArrayList<TopicNode<T>>();
-	        this.allChildren = new HashSet<T>();
+	        this.allChildren = new HashSet<String>();
 	    }
 
-	    public HashSet<T> getAllChildren() {
+	    public HashSet<String> getAllChildren() {
 	    	return allChildren;
 	    }
 
-	    // whenever you add a child to this node, also add the child +
-	    // it's children to the allChildren hashset
-        public void addChild(TopicNode<T> child) {
-        	children.add(child);
+        public void addChild(TopicNode<String> child) {
         	allChildren.add(child.data);
-        	Iterator<T> iter = child.getAllChildren().iterator();
-        	while (iter.hasNext()) {
-        		allChildren.add(iter.next());
+        	if (!child.getAllChildren().isEmpty()) {
+        		Iterator<String> iter = child.getAllChildren().iterator();
+	        	while (iter.hasNext()) {
+	        		allChildren.add(iter.next());
+	        	}
         	}
         }
-
-        public boolean contains(String topic) {
-        	return allChildren.contains(topic);
-        }
-
-	    public void printTree(){
-	    	printTreeHelper(0);
-	    }
-
-	    public void printTreeHelper(int n) {
-	    	for (int i = 0; i < n; i++) {
-	    		System.out.print("  ");
-	    	}
-	    	System.out.println(data);
-	    	for (int i = 0; i < children.size(); i++) {
-				children.get(i).printTreeHelper(n+1);
-			}
-	    }
-
 	}
 
 	private static class TrieNode {
-	    TrieNode[] array;
+		char val;
+	    HashMap<Character, TrieNode> set;
 	    HashMap<String, Integer> topics;
 
-	    public TrieNode() {
-	    	// array[27] ==> ' '
-	    	// array[28] ==> '?'
-	        this.array = new TrieNode[57];
+		public TrieNode() {
+	        this.set = new HashMap<Character, TrieNode>();
+	        this.topics = new HashMap<String, Integer>();
+	    }
+
+	    public TrieNode(char v) {
+	    	this.val = v;
+	        this.set = new HashMap<Character, TrieNode>();
 	        this.topics = new HashMap<String, Integer>();
 	    }
 
@@ -115,15 +92,13 @@ public class Ontology {
 
 	        for(int i = 0; i < word.length(); i++){
 	            char c = word.charAt(i);
-	            int index = c - 'A';
-	            if (c == ' ') { index = 27; }
-	            if (c == '?') { index = 28; }
-	            if(pointer.array[index] == null){
-	                TrieNode temp = new TrieNode();
-	                pointer.array[index] = temp;
-	                pointer = temp;
+
+	            if(pointer.set.containsKey(c)){
+	                pointer = pointer.set.get(c);
 	            } else {
-	                pointer = pointer.array[index];
+	                TrieNode t = new TrieNode(c);
+	                pointer.set.put(c, t);
+	                pointer = t;
 	            }
 	            pointer.addTopic(topic);
 	        }
@@ -131,16 +106,16 @@ public class Ontology {
 	 
 	    // if text doesn't exist, return 0
 	    public Integer numAssociations(String topic, String text) {
-	    	TopicNode node = _map.get(topic);
+	    	HashSet<String> children = _map.get(topic);
 	        TrieNode found = searchNode(text);
 	        int count = 0;
-	        if(found == null){
+	        if(found == null || children == null ) {
 	            return count;
 	        } else {
 	            Iterator<String> iter = found.topics.keySet().iterator();
 	            while (iter.hasNext()) {
 	            	String check = iter.next();
-	            	if (check.equals(node.data) || node.contains(check)) {
+	            	if (check.equals(topic) || children.contains(check)) {
 	            		count+=found.topics.get(check);
 	            	}
 	            }
@@ -151,30 +126,26 @@ public class Ontology {
 	    // returns the node based off the text, otherwise will return null
 	    public TrieNode searchNode(String text){
 	        TrieNode pointer = root;
-	        for(int i=0; i < text.length(); i++){
+
+	        for(int i = 0; i < text.length(); i++) {
 	            char c = text.charAt(i);
-	            int index = c-'A';
-	            if (c == ' ') { index = 27; }
-	            if (c == '?') { index = 28; }
-	            if(pointer.array[index]!=null){
-	                pointer = pointer.array[index];
-	            }else{
-	                return null;
+	            pointer = pointer.set.get(c);
+
+	            if (pointer == null) {
+	                return pointer;
 	            }
 	        }
-	        if(pointer == root)
-	            return null;
 	        return pointer;
 	    }
 	}
 
 	public static void main(String args[] ) throws Exception {
+		long startTime = System.currentTimeMillis();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    // line 1			int N
 	    int n = Integer.parseInt(reader.readLine());
-	    // System.out.println("N = "+n);
 	    // line 2			flat tree of N topics
-	    _map = new HashMap<String, TopicNode<String>>();
+	    _map = new HashMap<String, HashSet<String>>();
 	    makeTree(reader.readLine());
 	    // _topics.printTree();
 	    // line 3			int M
@@ -194,6 +165,8 @@ public class Ontology {
 	    	System.out.println(_trie.numAssociations(topicQuery[0], topicQuery[1]));
 	    	k--;
 	    }
+		long endTime = System.currentTimeMillis();
+		System.out.println("Took " + (endTime-startTime) + " milliseconds.");
     }
 }
 
